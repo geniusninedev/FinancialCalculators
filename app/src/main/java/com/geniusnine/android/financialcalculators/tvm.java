4cdaac19c6ed4d2eae27e28dc85e46f9599c558b
geniusninedev <developer@nineinfosys.com>
2017-02-08 19:22:24 +0530

    working on time value money activity

--- app/src/main/java/com/geniusnine/android/financialcalculators/tvm.java
+++ app/src/main/java/com/geniusnine/android/financialcalculators/tvm.java
@@ -154,6 +154,7 @@ public class tvm  extends AppCompatActivity {
         // Checks the orientation of the screen
         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
             Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
         } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
             Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
         }
